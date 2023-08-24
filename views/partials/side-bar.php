25444d07d0bdc6c1a55302c294b928dc61d84f41
Alexotieno1717 <alexotieno900@gmail.com>
2023-08-24 17:26:12 +0300

    :sparkles::recycle: Added option of downloading in csv and recycle the code to display in gris

--- views/partials/side-bar.php
+++ views/partials/side-bar.php
@@ -1,31 +1,16 @@
 <!-- Sidebar -->
-<nav
-    id="sidebarMenu"
-    class="collapse d-lg-block sidebar collapse bg-white"
->
+<nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
     <div class="position-sticky">
         <div class="list-group list-group-flush mx-3 mt-4">
-            <a
-                href="/create"
-                class="<?= urlIs('/create') ? 'active' : '' ?> list-group-item list-group-item-action py-2 ripple"
-            >
-                <i class="fas fa-circle-plus fa-fw me-3"></i
-                ><span>Add Branches</span>
+            <a href="/create" class="<?= urlIs('/create') ? 'active' : '' ?> list-group-item list-group-item-action py-2 ripple">
+                <i class="fas fa-circle-plus fa-fw me-3"></i><span>Add Branches</span>
             </a>
 
-            <a
-                    href="/"
-                    class="<?= urlIs('/') ? 'active' : '' ?> list-group-item list-group-item-action py-2 ripple"
-            >
-                <i class="fas fa-list fa-fw me-3"></i
-                ><span>Branches List</span>
+            <a href="/" class="<?= urlIs('/') ? 'active' : '' ?> list-group-item list-group-item-action py-2 ripple">
+                <i class="fas fa-list fa-fw me-3"></i><span>Branches List</span>
             </a>
-            <a
-                    href="/filter"
-                    class="<?= urlIs('/filter') ? 'active' : '' ?> list-group-item list-group-item-action py-2 ripple"
-            >
-                <i class="fas fa-list fa-fw me-3"></i
-                ><span>ussd</span>
+            <a href="/filter" class="<?= urlIs('/filter') ? 'active' : '' ?> list-group-item list-group-item-action py-2 ripple">
+                <i class="fas fa-list fa-fw me-3"></i><span>USSD Logs</span>
             </a>
         </div>
     </div>
